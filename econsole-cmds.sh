266dbdc575b7bb9d5f4322295f1b548926a3f8f4
david.ryder@appdynamics.com <David.Ryder@AppDynamics.com>
2019-05-31 12:31:26 -0700

    Multi install updatets, accountInfo, install/delete-all.sh

--- econsole-cmds.sh
+++ econsole-cmds.sh
@@ -12,37 +12,84 @@
 #   addRemoteHost
 #   installPrimaryController
 #   installSecondaryController
-#   installEventsService
+#   installSingleEventsService
+#   installMultiEventsService
 #   showParams
 #
 # Requires:
 #    econsole-envvars.sh
 #
 
-# Required environment variables
-declare -a \
-envVarList=("APPD_CONTROLLER_HOST1" "APPD_CONTROLLER_HOST2" \
-            "APPD_EVENTS_SERVICE_HOST1" "APPD_EVENTS_SERVICE_HOST2" "APPD_EVENTS_SERVICE_HOST3" \
-            "APPD_CONTROLLER_PORT" \
-            "APPD_CONTROLLER_INSTALL_DIR" "APPD_SSH_PRI_KEY_FILE" \
-            "APPD_SSH_CREDENTIAL_NAME" "APPD_SSH_USER_NAME" "APPD_CONTROLLER_NAME" \
-            "APPD_CONTROLLER_MODE" "APPD_CONTROLLER_PROFILE" "APPD_CONTROLLER_ADMIN" \
-            "APPD_UNIVERSAL_PWD" "APPD_LICENSE_FILE" "APPD_EVENTSSERVICE_PROFILE")
-[ -z "$APPD_VERSION" ]  && { echo "APPD_VERSION parameter not set"; exit 0; }
-[ -z "$APPD_RELEASE_NUMBER" ]  && { echo "APPD_RELEASE_NUMBER parameter not set"; exit 0; }
-EXIT_STATUS="1"
-for evar in "${envVarList[@]}"; do
-   [ -z ${!evar} ] && { echo "Environment variable not set: $evar"; EXIT_STATUS="0"; }
-done
-[ $EXIT_STATUS = "0" ] && { echo "Exiting"; exit 0; }
 
 
 cmd=${1:-"unknown"}
 
+# Temp files
+CURL_SESSION_FILE=/tmp/appd-curl-session.dat
 #
 # APPD_CURRENT_PLATFORM = APPD_CONTROLLER_NAME
 #
 #APPD_PLATFORM_ADMIN_CMD=$APPD_CONTROLLER_INSTALL_DIR/platform/platform-admin/bin/platform-admin.sh
+
+_validateEnvironmentVars() {
+  echo "Validating environment variables for $1"
+  shift 1
+  VAR_LIST=("$@") # rebuild using all args
+  #echo $VAR_LIST
+  for i in "${VAR_LIST[@]}"; do
+     [ -z ${!i} ] && { echo "Environment variable not set: $i"; ERROR="1"; }
+  done
+  [ "$ERROR" == "1" ] && { echo "Exiting"; exit 1; }
+}
+
+_controllerAutheticate() {
+  # Authentication to Controller
+  _validateEnvironmentVars "Authentication to controller" \
+        "APPD_CONTROLLER_ADMIN" "APPD_ACCOUNT" "APPD_UNIVERSAL_PWD" \
+        "APPD_CONTROLLER_HOST" "APPD_CONTROLLER_PORT"
+
+  APPD_FULL_USER=$APPD_CONTROLLER_ADMIN@$APPD_ACCOUNT:$APPD_UNIVERSAL_PWD
+  #B64AUTH=`echo $APPD_FULL_USER | base64`
+  rm -f $CURL_SESSION_FILE
+  curl $VERBOSE -s -c $CURL_SESSION_FILE \
+       --user "$APPD_FULL_USER" \
+       -X GET http://$APPD_CONTROLLER_HOST:$APPD_CONTROLLER_PORT/controller/auth?action=login
+  #cat $CURL_SESSION_FILE
+  # Get X-CSRF-TOKEN
+  SED_TRIM_SPACES='sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//''
+  XCSRFTOKEN=`grep X-CSRF-TOKEN $CURL_SESSION_FILE | sed 's/^.*X-CSRF-TOKEN\s*//' | $SED_TRIM_SPACES`
+}
+
+_http() {
+  METHOD=$1
+  PROTOCOL=$2
+  SERVICE=$3
+  POST_DATA=$4
+  HTTP_RESULT=`curl $VERBOSE -s -b $CURL_SESSION_FILE \
+       --header "Origin: http://$APPD_CONTROLLER_HOST:$APPD_CONTROLLER_PORT" \
+       --header "Accept-Encoding: text" \
+       --header "Accept-Language: en-US,en;q=0.9" \
+       --header "X-CSRF-TOKEN: ${XCSRFTOKEN}" \
+       --header "Content-Type: application/vnd.appd.cntrl+json;v=1" \
+       --header "Accept: application/json, text/plain, */*" \
+       --header "Referer: http://$APPD_CONTROLLER_HOST:$APPD_CONTROLLER_PORT/controller/" \
+       --data-binary "${POST_DATA}" \
+       --header "Connection: keep-alive" \
+       -X $METHOD $PROTOCOL://$APPD_CONTROLLER_HOST:$APPD_CONTROLLER_PORT$SERVICE`
+}
+
+
+
+# Required environment variables for install
+_validateEnvironmentVars "Controller Install" \
+            "APPD_CONTROLLER_HOST1" "APPD_CONTROLLER_HOST2" \
+            "APPD_EVENTS_SERVICE_HOST1" "APPD_EVENTS_SERVICE_HOST2" "APPD_EVENTS_SERVICE_HOST3" \
+            "APPD_CONTROLLER_PORT" \
+            "APPD_CONTROLLER_INSTALL_DIR" "APPD_SSH_PRI_KEY_FILE" \
+            "APPD_SSH_CREDENTIAL_NAME" "APPD_SSH_USER_NAME" "APPD_PLATFORM_NAME" \
+            "APPD_CONTROLLER_MODE" "APPD_CONTROLLER_PROFILE" "APPD_CONTROLLER_ADMIN" \
+            "APPD_UNIVERSAL_PWD" "APPD_LICENSE_FILE" "APPD_EVENTSSERVICE_PROFILE"  \
+            "APPD_VERSION" "APPD_RELEASE_NUMBER"
 
 #####################################
 # Create a Platform
@@ -68,12 +115,12 @@ elif [ $cmd == "addCredentials" ]; then
     echo "Private key $APPD_SSH_PRI_KEY_FILE is valid"
   fi
 
-    # Add credentials
-    $APPD_PLATFORM_ADMIN_CMD add-credential \
-      --credential-name $APPD_SSH_CREDENTIAL_NAME \
-      --user-name $APPD_SSH_USER_NAME \
-      --ssh-key-file $APPD_SSH_PRI_KEY_FILE \
-      --platform-name $APPD_PLATFORM_NAME
+  # Add credentials
+  $APPD_PLATFORM_ADMIN_CMD add-credential \
+    --credential-name $APPD_SSH_CREDENTIAL_NAME \
+    --user-name $APPD_SSH_USER_NAME \
+    --ssh-key-file $APPD_SSH_PRI_KEY_FILE \
+    --platform-name $APPD_PLATFORM_NAME
 
 elif [ $cmd == "addLocalHost" ]; then
   HOST_NAME=${2:-"Host_Name_Missing"}
@@ -87,15 +134,6 @@ elif [ $cmd == "addRemoteHost" ]; then
     $APPD_PLATFORM_ADMIN_CMD add-hosts --hosts $HOST_NAME \
       --credential $APPD_SSH_CREDENTIAL_NAME \
       --platform-name $APPD_PLATFORM_NAME
-
-
-#       destinationDirectory=$APPD_CONTROLLER_INSTALL_DIR \
-# platform-admin.sh submit-job --service controller --job install
-# --args controllerPrimaryHost=localhost controllerAdminUsername=<user1>
-# controllerAdminPassword=<password>
-# controllerRootUserPassword=<rootpassword>
-# mysqlRootPassword=<dbrootpassword> controllerDBPassword=<password>
-# controllerDBHost=<host> controllerProfile=<profile>
 
 #####################################
 # Install a Controller
@@ -123,7 +161,7 @@ elif [ $cmd == "installSecondaryController" ]; then
     $APPD_PLATFORM_ADMIN_CMD submit-job --service controller --job install \
       --platform-name $APPD_PLATFORM_NAME  \
       --args \
-      controllerPrimaryHost=$APPD_CONTROLLER_HOST2 \
+      controllerPrimaryHost=$APPD_CONTROLLER_HOST1 \
       controllerSecondaryHost=$APPD_CONTROLLER_HOST2 \
       destinationDirectory=$APPD_CONTROLLER_INSTALL_DIR \
       controllerProfile=$APPD_CONTROLLER_PROFILE \
@@ -185,6 +223,35 @@ elif [ $cmd == "enableEventsService" ]; then
     grep ad.bizoutcome.enabled $MFILE
     # events-service/processor/conf/events-service-api-store.properties
     # ad.bizoutcome.enabled=true
+
+elif [ $cmd == "accountInfo" ]; then
+  _controllerAutheticate
+  _http "GET" "http" "/controller/restui/user/account" ""
+
+  # Setup envvars
+  APPDYNAMICS_CONTROLLER_HOST_NAME=$APPD_CONTROLLER_HOST
+  APPDYNAMICS_CONTROLLER_PORT=$APPD_CONTROLLER_PORT
+  APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=`echo $HTTP_RESULT | jq -r .account.accessKey`
+  APPDYNAMICS_AGENT_ACCOUNT_NAME=`echo $HTTP_RESULT | jq -r .account.name`
+  APPDYNAMICS_GLOBAL_ACCOUNT_NAME=`echo $HTTP_RESULT | jq -r .account.globalAccountName`
+
+  # Export envvars for agent configuration
+
+  echo "# Agent configuration"
+  echo "export APPDYNAMICS_CONTROLLER_HOST_NAME=$APPDYNAMICS_CONTROLLER_HOST_NAME"
+  echo "export APPDYNAMICS_CONTROLLER_PORT=$APPDYNAMICS_CONTROLLER_PORT"
+  echo "export APPDYNAMICS_CONTROLLER_SSL_ENABLED=false"
+  echo "export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"
+  echo "export APPDYNAMICS_AGENT_ACCOUNT_NAME=$APPDYNAMICS_AGENT_ACCOUNT_NAME"
+  echo "export APPDYNAMICS_GLOBAL_ACCOUNT_NAME=$APPDYNAMICS_GLOBAL_ACCOUNT_NAME"
+  echo "export APPDYNAMICS_ANALYTICS_AGENT_URL=http://localhost:9090/v2/sinks/bt"
+  echo "export APPDYNAMICS_EVENTS_SERVICE_ENDPOINT=http://<----URL---->:8090"
+  echo "export APPDYNAMICS_SIM_ENABLED=true"
+
+  echo "# App, Tier, Node  Names"
+  echo "export APPDYNAMICS_AGENT_APPLICATION_NAME=APP_TEST_1"
+  echo "export APPDYNAMICS_AGENT_TIER_NAME=APP_TIER_T1"
+  echo "export APPDYNAMICS_AGENT_NODE_NAME=APP_NODE_N1"
 
 
 #####################################
--- envvars-example-1.sh
+++ envvars-example-1.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 #
-# Environment variables for econsole-cmds.sh for installation of
-# Controller and Events Service
-# Ver 4.5.7.18512
+# Environment vars for Platform Admin, Controller and Event Service Install
 #
+# Version 4.5.7.18512 of software to install
+export APPD_VERSION=4.5.7
+export APPD_RELEASE_NUMBER=18512
+
+# Platform Name for deployment of this controller and events service
+export APPD_PLATFORM_NAME=TEST1
+
+# Base directory for install, consistent across econsole, controller and events service
+export APPD_BASE_DIR=/home/appduser/appdynamics
+export APPD_CONTROLLER_INSTALL_DIR=$APPD_BASE_DIR/$APPD_PLATFORM_NAME
+export APPD_PLATFORM_ADMIN_CMD=$APPD_BASE_DIR/platform/platform-admin/bin/platform-admin.sh
+
+# Hostname of controller for authentication (same host as APPD_CONTROLLER_HOST1)
+export APPD_CONTROLLER_HOST=controller1.test1.com
+
+# Hostnames resolvable from econsole
 export APPD_CONTROLLER_HOST1=controller1
 export APPD_CONTROLLER_HOST2=controller2
 export APPD_EVENTS_SERVICE_HOST1=events1
 export APPD_EVENTS_SERVICE_HOST2=events2
 export APPD_EVENTS_SERVICE_HOST3=events3
-export APPD_BASE_DIR=/home/appduser/appdynamics
-export APPD_VERSION=4.5.7
-export APPD_RELEASE_NUMBER=18512
-export APPD_CONTROLLER_NAME1=controller1
-export APPD_CONTROLLER_NAME2=controller2
-export APPD_PLATFORM_NAME=test1
-export APPD_PLATFORM_ADMIN_CMD=$APPD_BASE_DIR/platform/platform-admin/bin/platform-admin.sh
-export APPD_CONTROLLER_INSTALL_DIR=$APPD_BASE_DIR/$APPD_CONTROLLER_NAME
+
+# Naming of controllers within econsole
+export APPD_CONTROLLER_NAME1=controllerA
+export APPD_CONTROLLER_NAME2=controllerB
+
 export APPD_CONTROLLER_PORT=8090
-export APPD_SSH_PRI_KEY_FILE=~/.ssh/02052019
-export APPD_SSH_CREDENTIAL_NAME=02052019
+
+# SSH passwordless access across nodes: RSA
+export APPD_SSH_PRI_KEY_FILE=~/.ssh/appduser-02052019
+export APPD_SSH_CREDENTIAL_NAME=appduser-02052019
 export APPD_SSH_USER_NAME=appduser
+
+# Controller authentication
+export APPD_CONTROLLER_ADMIN=admin
+export APPD_UNIVERSAL_PWD=changepwd1
+
+# Deployment sizing
 export APPD_CONTROLLER_MODE=single
 export APPD_CONTROLLER_PROFILE=small
-export APPD_CONTROLLER_ADMIN=admin
-export APPD_UNIVERSAL_PWD=welcome1
+export APPD_EVENTSSERVICE_PROFILE=dev
+
+# License file
 export APPD_LICENSE_FILE=license.lic
-export APPD_EVENTSSERVICE_PROFILE=prod
