9ddf6c57745114ed945abd517982be7686007bed
david.ryder@appdynamics.com <David.Ryder@AppDynamics.com>
2019-06-03 12:05:34 -0700

    single controller and events

--- install-all.sh
+++ install-all.sh
@@ -9,11 +9,11 @@
 ./econsole-cmds.sh addCredentials
 
 ./econsole-cmds.sh addRemoteHost $APPD_CONTROLLER_HOST1
-./econsole-cmds.sh addRemoteHost $APPD_CONTROLLER_HOST2
+#./econsole-cmds.sh addRemoteHost $APPD_CONTROLLER_HOST2
 
 ./econsole-cmds.sh addRemoteHost $APPD_EVENTS_SERVICE_HOST1
-./econsole-cmds.sh addRemoteHost $APPD_EVENTS_SERVICE_HOST2
-./econsole-cmds.sh addRemoteHost $APPD_EVENTS_SERVICE_HOST3
+#./econsole-cmds.sh addRemoteHost $APPD_EVENTS_SERVICE_HOST2
+#./econsole-cmds.sh addRemoteHost $APPD_EVENTS_SERVICE_HOST3
 
 ./econsole-cmds.sh installPrimaryController
 
